185f2fe9910768964727808ce0c1872aa9035522
Hong-Eungyo <vjvlaos75@naver.com>
2023-12-22 18:01:28 +0900

    20231222
    JS 작성 - 졌는 줄 알았는데 이겼다

--- js/test_01.js
+++ js/test_01.js
@@ -9,12 +9,27 @@ const about_box = document.querySelector('.about_content');
 
 document.addEventListener('scroll', () => {
     // console.log(window.scrollY);    
-    if (window.scrollY > line - 5) {
+    if (window.scrollY > line) {
         about_box.classList.add('display');
     } else {
         about_box.classList.remove('display');
     }
 
 });
+
+const imgs = document.querySelectorAll('.testimonial_img');
+const text = document.querySelectorAll('.testimonial_bubble');
+
+for (const img of imgs) {
+    img.addEventListener('click', (e)=> {
+        if(e.target == imgs.item(0)){
+            text.item(0).style.color = 'red';
+        } else if (e.target == imgs.item(1)) {
+            text.item(1).style.color = 'red';
+        } else {
+            text.item(2).style.color = 'red';
+        }
+    })
+}
 
 
